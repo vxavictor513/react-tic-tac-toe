94545152de82f1c3dd4d9e20cc3a6ddc4bfd3a4f
Wai Loon <wai.loon.theng@accenture.com>
2020-05-10 18:07:05 +0800

    Various enhancements

--- src/index.js
+++ src/index.js
@@ -5,50 +5,46 @@ import "./index.css";
 // In React, function components are a simpler way to write components that only contain a render method and don’t have their own state.
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button
+      className="square"
+      onClick={props.onClick}
+      style={{ backgroundColor: props.backgroundColor }}
+    >
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
+  // ref: https://www.telerik.com/blogs/beginners-guide-loops-in-react-jsx
+  renderRow() {
+    let content = [];
+    for (let i = 0; i < 3; i++) {
+      content.push(<div className="board-row">{this.renderSquare(3 * i)}</div>);
+    }
+    return content;
   }
 
   renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
+    let content = [];
+    for (let j = 0 + i; j < 3 + i; j++) {
+      content.push(
+        <Square
+          value={this.props.squares[j]}
+          onClick={() => this.props.onClick(j)}
+          backgroundColor={
+            this.props.winningCombo && this.props.winningCombo.includes(j)
+              ? "coral"
+              : "transparent"
+          }
+        />
+      );
+    }
+    return content;
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.renderRow()}</div>;
   }
 }
 
@@ -66,7 +62,10 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        combo: lines[i],
+      };
     }
   }
   return null;
@@ -79,11 +78,13 @@ class Game extends React.Component {
       history: [
         {
           squares: Array(9).fill(null),
+          stepColumn: null,
+          stepRow: null,
         },
       ],
       stepNumber: 0,
       xIsNext: true,
-      isEnded: false,
+      isEnded: null,
     };
   }
 
@@ -100,17 +101,58 @@ class Game extends React.Component {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
+    const afterStep = calculateWinner(squares);
     this.setState({
       // Unlike the array push() method you might be more familiar with, the concat() method doesn’t mutate the original array, so we prefer it.
       history: history.concat([
         {
           squares: squares,
+          stepColumn: this.translateColumn(i),
+          stepRow: this.translateRow(i),
         },
       ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
-      isEnded: calculateWinner(squares) != null,
+      isEnded: afterStep ? afterStep.combo : null,
     });
+  }
+
+  translateColumn(i) {
+    switch (i) {
+      case 0:
+      case 3:
+      case 6:
+        return 1;
+      case 1:
+      case 4:
+      case 7:
+        return 2;
+      case 2:
+      case 5:
+      case 8:
+        return 3;
+      default:
+        alert("Unexpected step");
+    }
+  }
+
+  translateRow(i) {
+    switch (i) {
+      case 0:
+      case 1:
+      case 2:
+        return 1;
+      case 3:
+      case 4:
+      case 5:
+        return 2;
+      case 6:
+      case 7:
+      case 8:
+        return 3;
+      default:
+        alert("Unexpected step");
+    }
   }
 
   jumpTo(step) {
@@ -120,7 +162,7 @@ class Game extends React.Component {
     });
     if (this.state.isEnded && step < this.state.history.length - 1) {
       this.setState({
-        isEnded: false,
+        isEnded: null,
       });
     }
   }
@@ -128,19 +170,32 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winningStatus = calculateWinner(current.squares);
+    const winner = winningStatus ? winningStatus.winner : null;
 
     const moves = history.map((step, move) => {
       const desc = move ? "Go to move #" + move : "Go to game start";
+      const coordinate = move
+        ? (move % 2 === 0 ? "O" : "X") +
+          ": (" +
+          step.stepColumn +
+          "," +
+          step.stepRow +
+          ")"
+        : "";
+      const fontWeight = move === this.state.stepNumber ? "bold" : "normal";
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <span style={{ fontWeight: fontWeight }}>{coordinate}</span>
         </li>
       );
     });
 
     let status;
-    if (winner) {
+    if (history.length === 10) {
+      status = "Result: Draw";
+    } else if (winner) {
       status = "Winner: " + winner;
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
@@ -152,6 +207,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winningCombo={winningStatus ? winningStatus.combo : null}
           />
         </div>
         <div className="game-info">
